a4dae562db1639950cee3bfefec7314a6a95d445
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-07-09 15:44:18 +0100

    Refactor to functions

--- run.py
+++ run.py
@@ -7,14 +7,20 @@ import time
 INTERVAL = 1800
 WITH_BDEPS = False
 
-while True:
-    if WITH_BDEPS:
+
+def update_status(with_bdeps: bool) -> list[str]:
+    if with_bdeps:
         command = ["emerge", "-NupDq", "--with-bdeps", "y", "world"]
     else:
         command = ["emerge", "-NupDq", "world"]
 
     output = subprocess.run(command, capture_output=True).stdout.decode().split("\n")
     updates = [line for line in output if "ebuild" in line or "binary" in line]
+
+    return updates
+
+
+def get_json(updates: list[str]) -> dict[str, str]:
     n_updates = len(updates)
 
     if n_updates > 0:
@@ -28,6 +34,17 @@ while True:
         text = ""
 
     output = {"class": class_, "alt": class_, "text": text, "tooltip": tooltip}
-    print(json.dumps(output), flush=True)
 
-    time.sleep(INTERVAL)
+    return output
+
+
+def main():
+    while True:
+        updates = update_status(with_bdeps=WITH_BDEPS)
+        output = get_json(updates=updates)
+        print(json.dumps(output), flush=True)
+        time.sleep(INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
